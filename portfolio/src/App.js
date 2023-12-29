08789375726b6f4ebafbc9ee0ead4f10f5299f28
Gabriel <felixgabriel.sant@gmail.com>
2023-12-29 14:22:57 -0300

    update

---
+++ portfolio/src/App.js
@@ -0,0 +1,23 @@
+import { Body } from "./Components/body"
+import { Flexing } from "./Components/socialbuttons"
+import { Dark } from "./Components/Dark"
+import { Container } from "./Components/Container"
+import { Login } from "./Components/Login"
+import { Begin } from "./Hotel/Begin"
+
+export default function App() {
+  return (
+      <div>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <br></br>
+        <Body/>
+        <div><br/>
+        <Flexing/>
+        </div><br />
+        <Dark/><br/>
+        <Container/><br />
+        <Login/>
+        <Begin/>
+      </div>      
+  )
+}
---
+++ portfolio/src/Hotel/Begin.js
@@ -0,0 +1,19 @@
+export function Begin(){
+    return(
+        <div class="h-screen">
+            <div class="container mx-auto px-4 h-24 flex items-center">
+                <div class="font-black">
+                    Logo
+                </div>
+                <ul class="ml-auto">
+                    <li>
+                        <a class="text-sm text-gray-600 hover:text-gray-800 hover:underline mr-2" href="">Reservas</a>
+                        <a class="text-sm text-gray-600 hover:text-gray-800 hover:underline mr-2" href="">Saiba mais</a>
+                        <a class="text-sm text-gray-600 hover:text-gray-800 hover:underline mr-2" href="">Informações</a>
+                        <a class="text-sm text-gray-600 hover:text-gray-800 hover:underline" href="">Contato</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
